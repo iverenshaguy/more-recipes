b9f516da0fe649e0efb324b9b778f6501b94f566
Iveren Shaguy <iverenshaguy@gmail.com>
2017-10-25 17:35:46 +0100

    Update Model Index File

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -12,9 +12,8 @@ const db = {};
 // if (config.use_env_variable) {
 //   sequelize = new Sequelize(process.env[config.use_env_variable]);
 // } else {
-const sequelize = new Sequelize(
-  config.database, config.username, config.password, config
-);
+
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 fs
   .readdirSync(__dirname)
