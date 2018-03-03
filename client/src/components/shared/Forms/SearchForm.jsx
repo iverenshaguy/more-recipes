a3af752786fef26cb6bb9618262c5044944af632
Iveren Shaguy <iverenshaguy@gmail.com>
2018-03-03 19:54:23 +0100

    :ok_hand: update code based on hound feedback

--- client/src/components/shared/Forms/SearchForm.jsx
+++ client/src/components/shared/Forms/SearchForm.jsx
@@ -1,46 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import FontAwesome from 'react-fontawesome';
 import { InputGroup, InputGroupButton, Button, Form, Label, Input } from 'reactstrap';
 
 /**
  * @exports
- * @class SearchForm
- * @classdesc Renders Search Form
+ * @function SearchForm
+ * @param {object} props
+ * @desc Renders Search Form
  * @returns {JSX} SearchForm
  */
-class SearchForm extends Component {
-  /**
-   * @memberof SearchForm
-   * @returns {JSX} SearchForm
-   */
-  render() {
-    const {
-      size, handleSearch, searchValue, handleSearchInput
-    } = this.props;
+const SearchForm = (props) => {
+  const {
+    size, handleSearch, searchValue, handleSearchInput
+  } = props;
 
-    return (
-      <Form id="search-form" onSubmit={handleSearch}>
-        <InputGroup size={size}>
-          <Label className="sr-only" for="search">
-            Search for Recipes
-          </Label>
-          <Input
-            id="search"
-            placeholder="Curried Chicken Gravy..."
-            onChange={handleSearchInput}
-            value={searchValue}
-          />
-          <InputGroupButton>
-            <Button type="submit" className="search-btn">
-              <FontAwesome name="search" />
-            </Button>
-          </InputGroupButton>
-        </InputGroup>
-      </Form>
-    );
-  }
-}
+  return (
+    <Form id="search-form" onSubmit={handleSearch}>
+      <InputGroup size={size}>
+        <Label className="sr-only" for="search">
+          Search for Recipes
+        </Label>
+        <Input
+          id="search"
+          placeholder="Curried Chicken Gravy..."
+          onChange={handleSearchInput}
+          value={searchValue}
+        />
+        <InputGroupButton>
+          <Button type="submit" className="search-btn">
+            <FontAwesome name="search" />
+          </Button>
+        </InputGroupButton>
+      </InputGroup>
+    </Form>
+  );
+};
 
 SearchForm.propTypes = {
   handleSearch: PropTypes.func.isRequired,
